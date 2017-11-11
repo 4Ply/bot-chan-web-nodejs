7752fed58dfeab062ee53ebf35ac55318442596a
Pawel Maskowicz <gamer.4ply@gmail.com>
2017-11-11 17:02:37 +0200

    Update runfile to only use first two CPUs

--- runfile.sh
+++ runfile.sh
@@ -4,17 +4,20 @@ docker stop bot-chan-web-nodejs
 
 docker run -d \
     --name bot-chan-web-mongodb \
+    --cpuset-cpus 0,1 \
     -v /docker_data/Bot-chan-web-nodejs/mongodb:/data/db \
     -p 27017:27017 \
     --restart="always" \
     mongo
 
-docker run -d --rm \
+docker run -d \
     --name bot-chan-web-nodejs \
+    --cpuset-cpus 0,1 \
     -e GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID \
     -e GOOGLE_CLIENT_SECRET=$GOOGLE_CLIENT_SECRET \
     -e BOT_CHAN_API_KEY_WEB=$BOT_CHAN_API_KEY_WEB \
     -e NODE_SESSION_SECRET=$NODE_SESSION_SECRET \
     -p 3001:3000 \
+    --restart="always" \
     --link bot-chan-web-mongodb:mongodb \
     bot-chan-web-nodejs
